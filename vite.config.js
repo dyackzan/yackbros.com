fc9ba5c958e9571306bdc0c69d16bddab7a53b5b
dwya222 <dwyackzan@gmail.com>
2025-06-15 13:17:31 -0400

    Vite deployment fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+export default {
+  base: "/", // GitHub Pages expects your repo name here if it's not a custom domain
+};
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// export default defineConfig({
+//   plugins: [react()],
+// })
